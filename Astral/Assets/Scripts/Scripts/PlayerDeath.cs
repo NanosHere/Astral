7019829bfb9a0edc760a20cd1c74d89657da9e25
NanosHere <71567368+NanosHere@users.noreply.github.com>
2023-10-10 13:42:34 -0400

    Revert "Apply player death"

--- Astral/Assets/Scripts/Scripts/PlayerDeath.cs
+++ Astral/Assets/Scripts/Scripts/PlayerDeath.cs
@@ -10,12 +10,9 @@ public class PlayerDeath : MonoBehaviour
 
     void OnTriggerEnter(Collider hit)
     {
-        Debug.Log("hit something");
-        if (hit.gameObject.tag == "Player")
+        if (hit.gameObject.tag == "killbox")
         {
-            //playerReset.position = new Vector3(0, 11, 73);
-
-            hit.gameObject.transform.position = playerReset.position;
+            playerReset.position = new Vector3(0, 11, 73);
         }
     }
 }
