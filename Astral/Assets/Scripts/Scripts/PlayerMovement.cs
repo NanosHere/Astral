2d4fbf154542a5c7da882f1e3a1161f0655a9e71
NanosHere <71567368+NanosHere@users.noreply.github.com>
2023-10-13 20:52:03 -0400

    fixed camera and added a maincameraPrefab

--- Astral/Assets/Scripts/Scripts/PlayerMovement.cs
+++ Astral/Assets/Scripts/Scripts/PlayerMovement.cs
@@ -66,20 +66,21 @@ public class PlayerMovement : MonoBehaviour
 
         //using inputs find the players rotation direction.
         Vector3 viewdir = transform.position - new Vector3(Camera.main.transform.position.x, transform.position.y, Camera.main.transform.position.z);
+        orientation.forward = viewdir;
         //orientation.forward = viewdir.normalized;
 
-        // Vector3 inputDir = orientation.forward * m_Input.z + orientation.right * m_Input.x;
+         Vector3 inputDir = orientation.forward * m_Input.z + orientation.right * m_Input.x;
 
 
         if(shootie.aimMode == false)
         {
 
        
-            //if(inputDir != Vector3.zero)
-            //{
+            if(inputDir != Vector3.zero)
+            {
                 //rotate player character;
-              //  playerChar.forward = Vector3.Slerp(playerChar.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
-            //}
+                playerChar.forward = Vector3.Slerp(playerChar.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+            }
         
          }
 
