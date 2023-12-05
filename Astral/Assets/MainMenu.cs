3790f15f9b45e90c926e881174af24afa7ed1296
NanosHere <71567368+NanosHere@users.noreply.github.com>
2023-12-05 12:37:12 -0500

    tree changes

--- Astral/Assets/MainMenu.cs
+++ Astral/Assets/MainMenu.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]Button main;
+    [SerializeField] Button control;
     public void playGame ()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void ToControls()
+    {
+        control.Select();
+    }
+    public void toMain()
+    {
+        main.Select();
     }
 }
