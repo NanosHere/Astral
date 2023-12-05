a009ce264a7f5469716d5b3ca72d10056d797625
NanosHere <71567368+NanosHere@users.noreply.github.com>
2023-12-05 13:22:11 -0500

    finalFixes

--- Astral/Assets/ToMainMenu.cs
+++ Astral/Assets/ToMainMenu.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ToMainMenu : MonoBehaviour
 {
+    [SerializeField] Button main;
     private void Start()
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+        main.Select();
     }
     public void ToMain()
     {
